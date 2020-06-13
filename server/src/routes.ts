f11d3a207c8db91288e2b7b7f75521933b61e5a7
Eduardo <dudu.takeo96@gmail.com>
2020-06-13 15:28:38 -0300

    completed all knex migrations

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({message: "Hello World"})
+routes.get('/', (request, response) => {
+    return response.json({message: "Hello World"})
 })
 
 export default routes;
