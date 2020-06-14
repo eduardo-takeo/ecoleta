b23df006f3d685fa85c0dd1618625ba725e81d43
Eduardo <dudu.takeo96@gmail.com>
2020-06-14 13:16:31 -0300

    added fetch items from api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
+    interface Item {
+        id: number;
+        title: string;
+        image_url: string;
+    }
+
+    const [items, setItems] = useState<Item[]>([]);
+    
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        })
+    }, []);
+    
     return (
         <div id="page-create-point">
             <header>
@@ -96,30 +111,12 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
                     </ul>
                 </fieldset>
 
